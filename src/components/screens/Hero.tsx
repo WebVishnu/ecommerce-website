90da53b01b6be30923e6f096d945a457d92de539
anshkaran7 <karanchandravanshi217@gmail.com>
2024-09-30 01:43:28 +0530

    Adjust padding in Hero component for improved layout consistency

--- src/components/screens/Hero.tsx
+++ src/components/screens/Hero.tsx
@@ -8,7 +8,7 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 function Hero() {
   return (
-    <div className="relative w-full h-[30vh] md:h-[70vh] lg:h-[90vh]"> {/* Responsive height based on screen size */}
+    <div className="relative w-full h-[30vh] pt-10 md:pt-20 md:h-[70vh] lg:h-[90vh]"> {/* Responsive height based on screen size */}
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={0}
@@ -20,7 +20,7 @@ function Hero() {
       >
         {/* Slide 1 */}
         <SwiperSlide>
-          <div className="relative w-full h-full pt-10 md:pt-20">
+          <div className="relative w-full h-full ">
             <Image
               src="/main.jpg"
               alt="Main Image 1"
